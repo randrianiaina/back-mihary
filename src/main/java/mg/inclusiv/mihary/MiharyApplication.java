bbbefe9021ab1091a7cc7b6f5fda33944f1c4442
Percky <ainapercky@gmail.com>
2023-03-01 09:41:16 +0300

    first commit

--- src/main/java/mg/inclusiv/mihary/MiharyApplication.java
+++ src/main/java/mg/inclusiv/mihary/MiharyApplication.java
@@ -2,6 +2,7 @@ package mg.inclusiv.mihary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MiharyApplication {
@@ -9,5 +10,8 @@ public class MiharyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MiharyApplication.class, args);
     }
-
+    @GetMapping("/helloworld")
+    public String hello() {
+        return"Hello World!";
+    }
 }
