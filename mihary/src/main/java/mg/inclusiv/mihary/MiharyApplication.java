b65fb697b6b84f9b62ef8202b62ad54428f1e593
Nomenjanahary Herizo RANDRIANIAINA <menja.herizo@gmail.com>
2023-03-01 09:10:27 +0300

    creation du projet spring

---
+++ mihary/src/main/java/mg/inclusiv/mihary/MiharyApplication.java
@@ -0,0 +1,13 @@
+package mg.inclusiv.mihary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiharyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiharyApplication.class, args);
+    }
+
+}
